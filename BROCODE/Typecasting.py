d8f6b6cf2ff2dd4c4c95e3e25d3cb7c12b9226b2
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-05-31 04:46:49 +0530

    03

---
+++ BROCODE/Typecasting.py
@@ -0,0 +1,10 @@
+# Typecasting = the process of converting a variable from one data type to another str(),int(),float(),bool().
+
+name = "Jishnu"
+age = 18
+sgpa = 7.31
+is_student = True
+
+name = bool(name)
+
+print(name)
