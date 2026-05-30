c829592e1e30dca9fa589c47510b109295fe63c1
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-05-31 05:07:40 +0530

    # Exercise 02 = Shopping Cart Program.

---
+++ BROCODE/UserInput02.py
@@ -0,0 +1,10 @@
+# Exercise 02 = Shopping Cart Program.
+
+item = (input( "what do you wanna buy? : "))
+quantity = float(input("how many of those do you wanna buy? : "))
+price = float(input("how much does it cost? : "))
+
+TotalAmount = quantity*price
+
+print(f"The total amount to be paid by you is : {TotalAmount}")
+
