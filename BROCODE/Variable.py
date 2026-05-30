68098b18669306edd9de5471ca15629f23a58204
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-05-30 05:31:18 +0530

    learned what variables are

---
+++ BROCODE/Variable.py
@@ -0,0 +1,34 @@
+#Variable = A container for a value (string,integer,float,boolean)
+#           A variable behaves as if it was the value it contains.
+
+# String = it is series of characters ,we can include no too though
+first_name = "Jishnu"
+food = "chicken burger"
+email = "jishnu25csu341@ncuindia.edu"
+
+#print 
+
+print(first_name)
+print(food)
+print(email)
+
+#use of f_string
+
+print(f"Hello my brotha {first_name}")
+print(f"do you wanna have {food}")
+print(f"your email is {email}")
+
+#Integers
+
+month = 5
+date = 30
+year = 2026
+
+#float
+
+FirstSemCgpa = 7.31
+SecondSemCgpa = 6.789
+
+# boolean 
+
+AmIFuckedInSecondSemester = True
