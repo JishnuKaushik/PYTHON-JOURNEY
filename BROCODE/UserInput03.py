dc4e94f18aee1b11d10b4dc0ff0321a28e94a28e
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-05-31 05:25:25 +0530

    # Madlibs game # Word game where you create a story # By filling in blanks with random words

---
+++ BROCODE/UserInput03.py
@@ -0,0 +1,14 @@
+# Madlibs game
+# Word game where you create a story 
+# By filling in blanks with random words
+
+adjective1 = input("enter an adj (description): ")
+adjective2 = input("enter an adj (description): ")
+adjective3 = input("enter an adj (description): ")
+noun1 = input("enter a verb(ation): ")
+verb1 = input("enter a noun (person, place, thing): ")
+
+print(f"Today I Went to a {adjective1}zoo.")
+print(f"in and exhibit, i saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"i was {adjective3}")
