9c1273f1bc471906946efb0ae6a963d29225311c
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-05-31 05:42:05 +0530

    Arithmetics

---
+++ BROCODE/06_ArithmeticsAndMaths.py
@@ -0,0 +1,15 @@
+MonsterCan = 100
+
+#Arithmetic operations on it:
+#MonsterCan += 1
+#MonsterCan = MonsterCan - 1 #or
+#MonsterCan -= 1
+#MonsterCan = MonsterCan*100 #or
+#MonsterCan *= 100
+#MonsterCan = MonsterCan / 100 #or
+#MonsterCan /= 100
+#MonsterCan = MonsterCan ** 2 #or
+#MonsterCan **= 2
+
+remainder = MonsterCan % 2
+print(remainder)
