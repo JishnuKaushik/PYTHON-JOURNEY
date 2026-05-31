9ec9238aad7b1840745af8738c19c047566a2c92
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-05-31 09:54:56 +0530

    if else elif

---
+++ BROCODE/07_IfStatementents.py
@@ -0,0 +1,9 @@
+# if = Do some code only if some condition is true 
+#      else do something else.
+
+age = int(input("umar kya hai bhai/bhen/lgbtq tere: "))
+
+if age >= 18 :
+    print("congrats you can sign up to porn hub")
+else :
+    print("abe jake pogo dekh")
