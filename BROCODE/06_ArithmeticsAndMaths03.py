4f877bf42bd38602ae086d8aac5c938ed14cfb64
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-05-31 09:29:53 +0530

    area and circumference of the circle

---
+++ BROCODE/06_ArithmeticsAndMaths03.py
@@ -0,0 +1,9 @@
+import math 
+
+radius = float(input("Radius bata bkl: "))
+
+circumference = 2 * math.pi * radius
+area = math.pi * pow(radius, 2)
+
+print(f"cirmuference of this gola is : {circumference}")
+print(f"area of this gola is : {area}")
