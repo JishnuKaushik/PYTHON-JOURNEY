6d39d2394afb52f16a27870ec04f06d39029f9bc
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-05-31 05:53:52 +0530

    round,abs,pow,max,min

---
+++ BROCODE/06_ArithmeticsAndMaths01.py
@@ -0,0 +1,11 @@
+x = 3.14
+y = -4 
+z = 5 
+
+# result = round(x) yeh round off karta hai 
+# result = abs(y) yeh number line pe 0 se whole no ke form mei jitna distance hota hai vo btata hai 
+# result = pow(4, 3) power function yha pe 4 is base and 3 is power.
+# result = max(x,y,z)
+result = min(x,y,z)
+
+print(result)
