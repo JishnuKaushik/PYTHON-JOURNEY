4844f5ab1a23fd39dfb5b1f373ef97fad7acbce6
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-06-05 13:53:20 +0530

    better

--- BROCODE/09_WeightConverter.py
+++ BROCODE/09_WeightConverter.py
@@ -6,12 +6,14 @@ unit = input(" kilograms or pounds ? :")
 if unit == "K" :
     weight = weight * 2.205
     unit = "LBS."
+    print(f"Your weight is : {weight} {unit}")
 
 elif unit == "L" :
     weight = weight / 2.205
     unit = "KGS."
+    print(f"Your weight is : {weight} {unit}")
 
 else : 
     print (f"{unit} was not valid" )
 
-print(f"Your weight is : {weight} {unit}")    
+    
