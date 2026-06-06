b475689d0a06ff10caa426c0d7c1992b41a15544
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-06-06 10:15:19 +0530

    Conditional Expression = A one- line shortcut for the if-else statement (ternary operator)                             Print or assign one of two values based on a Condition                              X if condition else Y

---
+++ BROCODE/12_ConditionalExpression.py
@@ -0,0 +1,23 @@
+"""Conditional Expression = A one- line shortcut for the if-else statement (ternary operator)
+                            Print or assign one of two values based on a Condition 
+                             X if condition else Y
+"""
+num = 5 
+a = 6
+b = 7
+age = 18 
+temperature = 40
+user_role = "guest"
+
+"""
+print("positive" is num > 0 else "negative" )
+result = "even" if num % 2 == 0 else "odd"
+max_num = a if a > b else b
+min_num = a if a < b else b
+status = "Adult" if age >= 18 else "minor"
+weather =  "hot" if temperature > 20 else "cold""
+access_level = "Full access" if user_role == "admin" else "Limited Access"
+
+print("access_level")
+
+"""
