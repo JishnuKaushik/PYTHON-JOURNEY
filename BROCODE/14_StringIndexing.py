5ffc535cdde725b63b758a12c3445d6d2f0324c8
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-06-10 17:54:06 +0530

    # indexing = accessing elements of a sequence using [] (indexing operator) #            [start : end : step]

---
+++ BROCODE/14_StringIndexing.py
@@ -0,0 +1,20 @@
+# indexing = accessing elements of a sequence using [] (indexing operator)
+#            [start : end : step]
+
+credit_number = "1234-5678-0912-3456"
+
+# print(credit_number[0])
+# print(credit_number[:4])
+# print(credit_number[5:9])
+# print(credit_number[5:])
+# print(credit_number[-1])
+# print(credit_number[::3])
+
+last_digits = credit_number[-4:]
+
+print(last_digits)
+
+# To reverse a string 
+
+credit_number = credit_number[::-1]
+print(credit_number)
