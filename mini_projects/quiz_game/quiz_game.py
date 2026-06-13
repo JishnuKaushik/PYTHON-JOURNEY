108c68914ca12a6cfdc45c14bc148e41f9b32702
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-06-13 17:31:02 +0530

    finished making quiz game

--- mini_projects/quiz_game/quiz_game.py
+++ mini_projects/quiz_game/quiz_game.py
@@ -9,7 +9,7 @@ questions = ("How many elements are in the periodic table ?: ",
 options = (("A. 116", "B. 117", "C. 118", "D. 119"),
            ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"),
            ("A. Nitrogen", "B. Oxygen", "C. Carbon-Dioxide", "D. Hydrogen"),
-           ("A. 206", "B. 207", "C. 208", "D. 209")
+           ("A. 206", "B. 207", "C. 208", "D. 209"),
            ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"))
 
 answers = ("C", "D", "A", "A", "B")
@@ -22,9 +22,32 @@ for question in questions:
     print(question)
 
     for option in options[question_num]:
-        print(option, end=" || ")
+        print(option)
         
-    guess = input("What do you think is crct ans to this question").upper()# we added .upper so that upper case and lower case dont cause any trouble.
-    guesses.append(guess)
+    guess = input("What do you think is crct ans to this question : ").upper()# we added .upper so that upper case and lower case dont cause any trouble.
+    guesses.append(guess) 
+    if guess == answers[question_num]:
+        score += 1
+        print("correct")
+
+    else:
+        print("incorrect")
+        print(f"{answers[question_num]} is the correct ans")    
     question_num += 1    
         
+print("--------------------------------------------------------------------")        
+print("-------------------------------RESULT-------------------------------")
+print("--------------------------------------------------------------------")
+
+print("answers: ", end="")
+for answer in answers:
+    print(answer, end="")
+print()
+
+print("guesses: ", end="")
+for guess in guesses:
+    print(guess, end="")
+print()
+
+score = int(score / len(questions) * 100)
+print(f"Your score is : {score}")
