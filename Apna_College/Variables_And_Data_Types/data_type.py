93292b2ea9c215d489dc368de1ce976c431e9808
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-06-18 19:30:44 +0530

    apna clg data types

---
+++ Apna_College/Variables_And_Data_Types/data_type.py
@@ -0,0 +1,17 @@
+""""
+Data type = 
+1. integers
+2. string
+3. float
+4. boolean
+5. none
+"""
+
+
+name = "Jishnu Kaushik" # string
+age = 18 # int
+dob = "16/11/2007" # string
+
+print(type(name))
+print(type(age))
+print(type(dob))
