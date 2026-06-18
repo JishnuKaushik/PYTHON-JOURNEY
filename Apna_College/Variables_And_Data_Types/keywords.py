db2cd79cb6537590c59260911eeb5df5169759d3
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-06-18 19:39:33 +0530

    apna clg keywords

---
+++ Apna_College/Variables_And_Data_Types/keywords.py
@@ -0,0 +1,37 @@
+# Keywords = Keywords are reserved words in python.
+
+""""
+1. and
+2. as
+3. assert
+4. break
+5. class
+6. continue
+7. def
+8. del
+9. elif
+10. else 
+11. except
+12. finally
+13. False
+14. for
+15. from
+16. global
+17. if
+18. import
+19. in
+20. is
+21. lambda
+22. nonlocal 
+23. None
+24. not
+25. or 
+26. pass
+27. raise
+28. return
+29. True
+30. try
+31. with
+32. while
+33. yield
+"""
