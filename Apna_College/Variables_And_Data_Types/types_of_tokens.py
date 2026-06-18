5f4a38d83c816ce56abcfd2362b8586a59c15af7
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-06-18 20:09:01 +0530

    apna clg ;Types of Tokens:

---
+++ Apna_College/Variables_And_Data_Types/types_of_tokens.py
@@ -0,0 +1,10 @@
+# Types of Tokens:
+# Punctuators = punctuators are symbols to organise sentence structure in programming.
+# 
+# (),{},@,[],etc
+# -=, += ,etc
+
+# Typed languages = jin languages mei data type hote hai 
+# it has two types => 
+# 1) Implicit -> jin programming language mei hamko data type nhi batana padta [ for eg = python ; hame python mei alag se nhi batana pdta ki ham konsa data type use kr rhe hai] 
+# 2) Explicit -> jin programming language mei hame data type mention karna padta hai [ for eg = c++, java ; we have to mention which data type we are using before variable name ]
